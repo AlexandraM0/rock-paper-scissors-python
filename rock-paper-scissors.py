14c91ac4ebc38c33b587de7d37998fe31bf2788d
AlexandraM0 <alexandra.moghina97@gmail.com>
2024-02-06 17:04:11 +0200

    Rock, paper, scissors

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -16,3 +16,22 @@ while True:
     random_number = random.randint(0,2)
     # rock: 0, paper: 1, scissors: 2 
     computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print ("You won!")
+        user_wins = user_wins + 1
+    
+    elif user_input == "paper" and computer_pick == "rock":
+        print ("You won!")
+        user_wins += 1
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print ("You won!")
+        user_wins += 1
+    
+    else:
+        print ("You lost!")
+        computer_wins += 1
+
+print("Goodbye!")
